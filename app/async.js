046dd4c18a1c6a7221b07ad71a8a79e3a857a580
Justin Barabander <jbarabander@gmail.com>
2016-07-27 14:18:34 -0400

    Working on async portion

--- app/async.js
+++ app/async.js
@@ -2,10 +2,26 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.asyncAnswers = {
   async: function(value) {
-
+    var promise = $.Deferred();
+    // if the value is an instance of an error the promise should reject
+    // and should pass the error value to the reject callbacks
+    if (Object.prototype.toString.call(value) === '[object Error]') {
+      promise.reject(value);
+    } else {
+      promise.resolve(value);
+    }
+    return promise;
   },
 
   manipulateRemoteData: function(url) {
-
+    return $.ajax({
+      url: url,
+      method: 'GET'
+    })
+    .then(function (data) {
+      return data.people.map(function (person) {
+        return person.name;
+      }).sort()
+    });
   }
 };
