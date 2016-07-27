7fe365fdfd784a8e206e6928714ddd59a27d144a
Justin Barabander <jbarabander@gmail.com>
2016-07-27 14:30:57 -0400

    Added in setTimeouts

--- app/async.js
+++ app/async.js
@@ -2,15 +2,19 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.asyncAnswers = {
   async: function(value) {
-    var promise = $.Deferred();
+    var deferred = $.Deferred();
     // if the value is an instance of an error the promise should reject
     // and should pass the error value to the reject callbacks
     if (Object.prototype.toString.call(value) === '[object Error]') {
-      promise.reject(value);
+      setTimeout(function () {
+        deferred.reject(value);
+      }, 50);
     } else {
-      promise.resolve(value);
+      setTimeout(function () {
+        deferred.resolve(value);
+      }, 50);
     }
-    return promise;
+    return deferred.promise();
   },
 
   manipulateRemoteData: function(url) {
